ea7a4f72d546f569f8ae4733a5e07b288085bdea
Ralf S. Engelschall <rse@engelschall.com>
2017-04-03 22:43:30 +0200

    initiaölly place under version control

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# gemstone-config
-Configuration Reader of Gemstone JavaScript Technology Stack
+
+Gemstone Config
+===============
+
+Configuration Reader of the
+[Gemstone JavaScript Technology Stack](http://gemstonejs.com).
+
+<p/>
+<img src="https://nodei.co/npm/gemstone-config.png?downloads=true&stars=true" alt=""/>
+
+<p/>
+<img src="https://david-dm.org/rse/gemstone-config.png" alt=""/>
+
+About
+-----
+
+This is a Node.js module for loading `gemstone.yaml` configuration files.
+
+Copyright &amp; License
+-----------------------
+
+Copyright &copy; 2016-2017 [GemstoneJS Project](http://gemstonejs.com)<br/>
+Licensed under [Apache License 2.0](https://spdx.org/licenses/Apache-2.0)
+
---
+++ gemstone-config.js
@@ -0,0 +1,141 @@
+/*
+**  GemstoneJS -- Gemstone JavaScript Technology Stack
+**  Copyright (c) 2016-2017 Gemstone Project <http://gemstonejs.com>
+**  Licensed under Apache License 2.0 <https://spdx.org/licenses/Apache-2.0>
+*/
+
+const fs     = require("fs-promise")
+const ducky  = require("ducky")
+const extend = require("extend")
+const jsYAML = require("js-yaml")
+
+module.exports = () => {
+    /*  default Gemstone configuration  */
+    let cfg = {
+        path: {
+            output: "./dst",
+            source: "./src",
+            main:   "index.js",
+            icon:   ""
+        },
+        meta: {
+            title:       "Example",
+            description: "",
+            author:      "",
+            keywords:    ""
+        },
+        header: "",
+        linting: {
+            eslint: {},
+            htmlhint: {},
+            stylelint: {}
+        },
+        modules: {
+            source: [],
+            provide: [],
+            replace: [],
+            alias: [],
+            rules: []
+        }
+    }
+
+    /*  validate a Gemstone configuration chunk  */
+    const validate = (filename, chunk) => {
+        let errors = []
+        if (!ducky.validate(chunk, `{
+            path?: {
+                output?: string,
+                source?: string,
+                main?:   string,
+                icon?:   string
+            },
+            meta?: {
+                title?:       string,
+                description?: string,
+                author?:      string,
+                keywords?:    string
+            },
+            header?: string,
+            linting?: {
+                eslint?: object,
+                htmlhint?: object,
+                stylelint?: object
+            },
+            modules?: {
+                source?: [
+                    string*
+                ],
+                provide?: [
+                    { name: string, require: string }*
+                ],
+                replace?: [
+                    { match: string, replace: (string | [ [ string, string ]* ] ) }*
+                ],
+                alias?: [
+                    { from: string, to: string }*
+                ],
+                rules?: [
+                    { test: string, use: object }*
+                ]
+            }
+        }`, errors))
+            throw new Error(`invalid Gemstone configuration chunk in "${filename}": ${errors.join("; ")}`)
+    }
+
+    /*  try to load Gemstone configuration chunks from YAML files  */
+    let filenames = [ "gemstone.yaml", ".gemstone.yaml" ]
+    filenames.forEach((filename) => {
+        if (fs.existsSync(filename)) {
+            let chunk
+            try {
+                chunk = jsYAML.safeLoad(fs.readFileSync(filename, "utf8"))
+            }
+            catch (ex) {
+                process.stderr.write(`gemstone-config ERROR: failed to parse YAML file "${filename}":\n`)
+                process.stderr.write(`${ex.message}\n`)
+                process.exit(1)
+            }
+            validate(filename, chunk)
+            extend(true, cfg, chunk)
+        }
+    })
+
+    /*  try to load Gemstone configuration chunks from JSON files  */
+    filenames = [ "gemstone.json", ".gemstone.json" ]
+    filenames.forEach((filename) => {
+        if (fs.existsSync(filename)) {
+            let chunk
+            try {
+                chunk = JSON.parse(fs.readFileSync(filename, "utf8"))
+            }
+            catch (ex) {
+                process.stderr.write(`gemstone-config ERROR: failed to parse JSON file "${filename}":\n`)
+                process.stderr.write(`${ex.message}\n`)
+                process.exit(1)
+            }
+            validate(filename, chunk)
+            extend(true, cfg, chunk)
+        }
+    })
+
+    /*  try to load Gemstone configuration chunks from NPM files  */
+    let filename = "package.json"
+    if (fs.existsSync(filename)) {
+        let chunk
+        try {
+            chunk = JSON.parse(fs.readFileSync(filename, "utf8"))
+        }
+        catch (ex) {
+            process.stderr.write(`gemstone-config ERROR: failed to parse JSON file "${filename}":\n`)
+            process.stderr.write(`${ex.message}\n`)
+            process.exit(1)
+        }
+        if (typeof chunk.gemstone === "object") {
+            validate(filename, chunk.gemstone)
+            extend(true, cfg, chunk.gemstone)
+        }
+    }
+
+    return cfg
+}
+
